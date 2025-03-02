33a9ae764d87ba8e5dd3a11fd8a0e4a283732587
Vyshnavi Kovvuru <137849211+vyshnavikovvuru@users.noreply.github.com>
2025-03-02 18:20:10 +0530

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -8,7 +8,7 @@ const {cloudinary} =require("../cloudinary")
 
 module.exports.index = async(req, res) => {
     const campgrounds = await Campground.find({})
-    res.render('campgrounds/index', {campgrounds })
+    res.render('campgrounds/index', {campgrounds,  maptilerApiKey: process.env.MAPTILER_API_KEY  })
 }
 
 module.exports.renderNewForm = (req,res) => {
